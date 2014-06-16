36b9a92d7d46c16244e1013ca25cce14cce3dcf6
login111 <mailforgit123@yandex.ru>
2014-06-16 15:00:19 +0300

    Добавлена отправка почты
    Требует доработок

--- CheckerApp/CheckerApp/EmailSender.cs
+++ CheckerApp/CheckerApp/EmailSender.cs
@@ -9,7 +9,13 @@ namespace CheckerApp
     {
         public void Sending()
         {
-            
+            System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+            message.To.Add("dest@gmail.com");
+            message.Subject = "Ошибка";
+            message.From = new System.Net.Mail.MailAddress("from@gmail.com");
+            message.Body = "Ошибка";
+            System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com");
+            smtp.Send(message);
         }
     }
 }
